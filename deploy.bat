179d211f9d3f375b23146d731bba3f43714471fb
Lichtung64 <jozhzhang@163.com>
2026-02-21 19:58:16 +0800

    Add missing article files

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,7 @@
 @echo off
 cd /d D:\软件\opencode\physics-site
-git init
 git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/Lichtung64/physics-site.git
-git push -u origin main
+git commit -m "Add missing article files"
+git push
 echo Done!
 pause
